64595729810072fdd706882910cbb377b2f75986
Mumu <linmumu0728@163.com>
2018-11-25 22:08:56 +0800

    .

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to my" @listenChild="reviceChildInfo"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    reviceChildInfo (val) {
+      console.log(val)
+    }
+  }
+}
+</script>
